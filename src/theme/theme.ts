9aa4ec6ea9a0072aefba0f7eb57f435a1251a7c9
remant <paudelremant45@gmail.com>
2025-04-26 12:30:05 +0545

    add theme configuration for Menu component

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    components: {
+      Menu: {
+        colorPrimary: '#000000',
+        itemPaddingInline: 20,
+        fontSize: 20,
+        fontWeight: 600,
+      }
+    },
+  };
+  
