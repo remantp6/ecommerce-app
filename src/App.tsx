c22b7adc871fab9d14e76cc5f65854de93057663
remant <paudelremant45@gmail.com>
2025-04-28 17:31:26 +0545

    feat: implement routing in App component and enhance main entry point for React 19 compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home-page";
+import Men from "./pages/men-page";
+import Women from "./pages/women-page";
+import Kids from "./pages/kids-page";
+import Cart from "./pages/cart-page";
+import Login from "./pages/login-page";
+import SignUp from "./pages/signup-page";
+import ForgetPassword from "./pages/forget-password-page";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import '@ant-design/v5-patch-for-react-19';
 import "./index.css";
 import App from "./App.tsx";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, unstableSetRender } from "antd";
 import { theme } from "./theme/theme.ts";
+
+// Extend the container type
+type ContainerWithRoot = Element & { _reactRoot?: ReturnType<typeof createRoot> };
+
+// Set the render function for React 19 and antd v5 compatibility and remove after major release
+unstableSetRender((node, container) => {
+  const typedContainer = container as ContainerWithRoot;
+  typedContainer._reactRoot ||= createRoot(container);
+  const root = typedContainer._reactRoot!;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
