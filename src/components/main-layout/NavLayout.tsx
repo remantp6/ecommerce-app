293997a172a56f5ae0ba00f2444364ac4e7344a7
remant <paudelremant45@gmail.com>
2025-05-03 19:50:08 +0545

    feat: add home menu item to NavLayout and update active tab logic

--- src/components/main-layout/NavLayout.tsx
+++ src/components/main-layout/NavLayout.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Layout, Menu, Drawer, Grid, Space, MenuProps } from "antd";
 import {
   CloseOutlined,
+  HomeOutlined,
   MenuOutlined,
   SearchOutlined,
   ShoppingCartOutlined,
@@ -27,6 +28,14 @@ const { useBreakpoint } = Grid;
 
 // menu items for the navbar
 const items: MenuItem[] = [
+  {
+    label: (
+      <Link to="/" className="!text-black">
+        <HomeOutlined style={{ fontSize: 24 }} />
+      </Link>
+    ),
+    key: "home",
+  },
   {
     label: (
       <Link to="/men" className="!text-black">
@@ -60,17 +69,22 @@ const NavLayout: React.FC<NavBarProps> = ({
 }) => {
   const [drawerVisible, setDrawerVisible] = useState(false);
   const screens = useBreakpoint();
-  const location = useLocation(); // ✅ Get current route
+  const location = useLocation(); //Get current route
 
-  // ✅ Determine active tab based on pathname
+  // Determine active tab based on pathname
   const currentPath = location.pathname;
-  const currentTab = currentPath.startsWith("/men")
+  const currentTab = currentPath.startsWith("/")
+    ? "home"
+    : currentPath.startsWith("/men")
     ? "men"
     : currentPath.startsWith("/women")
     ? "women"
     : currentPath.startsWith("/kids")
     ? "kids"
     : ""; // No active tab if not matched
+
+  const tabPaths = ["/", "/men", "/women", "/kids"];
+  const isMainTabPage = tabPaths.includes(currentPath);
 
   // function to handle menu item click
   const onClick: MenuProps["onClick"] = (e) => {
@@ -98,7 +112,7 @@ const NavLayout: React.FC<NavBarProps> = ({
                 mode="horizontal"
                 onClick={onClick}
                 items={items}
-                selectedKeys={currentTab ? [currentTab] : []}
+                selectedKeys={isMainTabPage && currentTab ? [currentTab] : []}
                 style={{ borderBottom: "none" }}
               />
             </div>
