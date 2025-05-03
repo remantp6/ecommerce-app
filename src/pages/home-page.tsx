be028b75cc063727f32e9732a3c4e52c1d26c45b
remant <paudelremant45@gmail.com>
2025-05-03 19:50:18 +0545

    feat: add ProductList component to Home page to display products

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,11 +1,14 @@
 import NavLayout from "../components/main-layout/NavLayout";
 import CustomCarousel from "../components/CustomCarousel";
 import TopHeader from "../components/TopHeader";
+import ProductList from "../components/product-list/ProductList";
+import {products} from "../data/products";
 
 const Home = () => {
   return (
     <NavLayout header={<TopHeader />} isNoAuthPage>
       <CustomCarousel />
+      <ProductList products={products}/>
     </NavLayout>
   );
 };
