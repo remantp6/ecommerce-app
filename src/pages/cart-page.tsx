9f0c5cfe20badc523510951c4788ef769a25b9a1
remant <paudelremant45@gmail.com>
2025-05-05 22:41:21 +0545

    feat: implement CartPage component with empty cart image and layout

--- src/pages/cart-page.tsx
+++ src/pages/cart-page.tsx
@@ -1,9 +1,16 @@
-const Cart = () => {
-  return (
-    <div>
-      <p>Cart</p>
-    </div>
-  );
-};
+import { Image } from "antd"
+import NavLayout from "../components/main-layout/NavLayout"
+import TopHeader from "../components/TopHeader"
+import emptyCart from '../assets/empty-cart.png'
 
-export default Cart;
+const CartPage = () => {
+  return (
+    <NavLayout header={<TopHeader />} isNoAuthPage>
+      <div className="min-h-[calc(100dvh-208px)] md:min-h-[calc(100dvh-170px)] flex items-center justify-center">
+      <Image preview={false} src={emptyCart} alt="empty-cart"/>
+      </div>
+    </NavLayout>
+  )
+}
+
+export default CartPage
