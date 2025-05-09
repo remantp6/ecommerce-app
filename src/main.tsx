4cc50d949e4e485c0d8116c98f5527738ef312a6
remant <paudelremant45@gmail.com>
2025-05-09 22:29:01 +0545

    feat: integrate React Query by adding QueryClientProvider to main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import '@ant-design/v5-patch-for-react-19';
+import "@ant-design/v5-patch-for-react-19";
 import "./index.css";
 import App from "./App.tsx";
 import { ConfigProvider, unstableSetRender } from "antd";
 import { theme } from "./theme/theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Extend the container type
-type ContainerWithRoot = Element & { _reactRoot?: ReturnType<typeof createRoot> };
+type ContainerWithRoot = Element & {
+  _reactRoot?: ReturnType<typeof createRoot>;
+};
+
+// Create a client
+const queryClient = new QueryClient();
 
 // Set the render function for React 19 and antd v5 compatibility and remove after major release
 unstableSetRender((node, container) => {
@@ -23,8 +29,10 @@ unstableSetRender((node, container) => {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider theme={theme}>
-      <App />
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider theme={theme}>
+        <App />
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
