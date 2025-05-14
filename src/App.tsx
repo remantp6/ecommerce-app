2ba725e72fee5c80449e8dc087ecb588ee382b43
remant <paudelremant45@gmail.com>
2025-05-14 23:47:36 +0545

    feat: add routes for OTP request and password creation pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import SignUp from "./pages/signup-page";
 import ForgetPassword from "./pages/forget-password-page";
 import VerifyEmail from "./pages/email-verify";
 import AddPassword from "./pages/add-password-page";
+import OtpPage from "./pages/otp-page";
+import PasswordCreatePage from "./pages/password-create-page";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/otp-request" element={<OtpPage />} />
+        <Route path="/password-create" element={<PasswordCreatePage />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/add-password" element={<AddPassword />} />
