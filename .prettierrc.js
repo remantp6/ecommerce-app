18a39b2cacd5cb10619235eb571438a988952ae4
remant <paudelremant45@gmail.com>
2025-05-22 13:31:05 +0545

    feat: add Prettier configuration files for code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	semi: true,
+	printWidth: 120,
+	useTabs: true,
+	tabWidth: 4,
+	endOfLine: 'lf',
+	trailingComma: 'all',
+};
