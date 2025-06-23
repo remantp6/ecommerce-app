daafc3ccfab4405395d37ce23ac100fc66b62844
remant <paudelremant45@gmail.com>
2025-06-23 23:37:03 +0545

    feat: add toast notifications to home page and remove unused button

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,50 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
-import { ErrorBoundary } from "./components/ErrorBoundary";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { Toaster } from 'react-hot-toast';
 
 // Lazy load all pages
-const Home = lazy(() => import("./pages/home-page"));
-const Men = lazy(() => import("./pages/men-page"));
-const Women = lazy(() => import("./pages/women-page"));
-const Kids = lazy(() => import("./pages/kids-page"));
-const Cart = lazy(() => import("./pages/cart-page"));
-const Login = lazy(() => import("./pages/login-page"));
-const SignUp = lazy(() => import("./pages/signup-page"));
-const ForgetPassword = lazy(() => import("./pages/forget-password-page"));
-const VerifyEmail = lazy(() => import("./pages/email-verify"));
-const AddPassword = lazy(() => import("./pages/add-password-page"));
-const OtpPage = lazy(() => import("./pages/otp-page"));
-const PasswordCreatePage = lazy(() => import("./pages/password-create-page"));
-const LoaderScreen = lazy(() => import("./components/LoaderScreen"));
-const NoMatch = lazy(() => import("./pages/no-match-page"));
-const ErrorFallback = lazy(() => import("./components/ErrorFallback"));
+const Home = lazy(() => import('./pages/home-page'));
+const Men = lazy(() => import('./pages/men-page'));
+const Women = lazy(() => import('./pages/women-page'));
+const Kids = lazy(() => import('./pages/kids-page'));
+const Cart = lazy(() => import('./pages/cart-page'));
+const Login = lazy(() => import('./pages/login-page'));
+const SignUp = lazy(() => import('./pages/signup-page'));
+const ForgetPassword = lazy(() => import('./pages/forget-password-page'));
+const VerifyEmail = lazy(() => import('./pages/email-verify'));
+const AddPassword = lazy(() => import('./pages/add-password-page'));
+const OtpPage = lazy(() => import('./pages/otp-page'));
+const PasswordCreatePage = lazy(() => import('./pages/password-create-page'));
+const LoaderScreen = lazy(() => import('./components/LoaderScreen'));
+const NoMatch = lazy(() => import('./pages/no-match-page'));
+const ErrorFallback = lazy(() => import('./components/ErrorFallback'));
 
 function App() {
-  return (
-    <BrowserRouter>
-    <ErrorBoundary fallback={<ErrorFallback/>}>
-      <Suspense
-        fallback={
-          <LoaderScreen/>
-        }
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/men" element={<Men />} />
-          <Route path="/women" element={<Women />} />
-          <Route path="/kids" element={<Kids />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/otp-request" element={<OtpPage />} />
-          <Route path="/password-create" element={<PasswordCreatePage />} />
-          <Route path="/forget-password" element={<ForgetPassword />} />
-          <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route path="/add-password" element={<AddPassword />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </Suspense>
-      </ErrorBoundary>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<ErrorBoundary fallback={<ErrorFallback />}>
+				<Suspense fallback={<LoaderScreen />}>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/men' element={<Men />} />
+						<Route path='/women' element={<Women />} />
+						<Route path='/kids' element={<Kids />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/signup' element={<SignUp />} />
+						<Route path='/otp-request' element={<OtpPage />} />
+						<Route path='/password-create' element={<PasswordCreatePage />} />
+						<Route path='/forget-password' element={<ForgetPassword />} />
+						<Route path='/verify-email' element={<VerifyEmail />} />
+						<Route path='/add-password' element={<AddPassword />} />
+						<Route path='*' element={<NoMatch />} />
+					</Routes>
+					<Toaster position='top-right' />
+				</Suspense>
+			</ErrorBoundary>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -3,16 +3,10 @@ import CustomCarousel from '../components/CustomCarousel';
 import TopHeader from '../components/TopHeader';
 import ProductList from '../components/product-list/ProductList';
 import { products } from '../data/products';
-import { Button } from 'antd';
-import toast, { Toaster } from 'react-hot-toast';
-
-const notify = () => toast('Here is your toast.');
 
 const Home = () => {
 	return (
 		<NavLayout header={<TopHeader />} isNoAuthPage>
-			<Button onClick={notify}>Check Toast</Button>
-			<Toaster />
 			<CustomCarousel />
 			<ProductList products={products} />
 		</NavLayout>
